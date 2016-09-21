86924431d13774fe3b38841d3a7280f59032a8d2
Joshua Goldbloom <Josh@Joshs-MacBook-Air.local>
2016-09-21 13:33:10 -0700

    all

---
+++ BamazonCustomer.js
@@ -0,0 +1,13 @@
+
+var argOne = process.argv[2]
+var argTwo = process.argv[3]
+
+
+if (argOne === "items for sale"){
+
+	
+	console.log("shirts", "shoes", "phones");
+}
+else if (argTwo = "price of each item"){
+	console.log("3.10", "49.00", "100");
+}
